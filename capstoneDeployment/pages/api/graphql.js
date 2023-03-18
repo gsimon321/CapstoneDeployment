dad9a65ac0c2f53387f51061c1c11562b9c1166f
GaborSimon <60612435+gsimon321@users.noreply.github.com>
2023-03-18 16:58:34 -0400

    fiyah mon

--- capstoneDeployment/pages/api/graphql.js
+++ capstoneDeployment/pages/api/graphql.js
@@ -1,53 +1,53 @@
-import  {  ApolloServer  }  from  "apollo-server-micro";
-import connectDb from './config/connectionDB'
+import { ApolloServer } from "apollo-server-micro";
+import { graphqlUploadExpress } from "graphql-upload";
+import connectDb from "./config/connectionDB";
 import processRequest from "graphql-upload/processRequest.mjs";
-console.log(ApolloServer)
 
-console.log(processRequest)
-
-const typeDefs = require("./schemas/typdefs")
-const resolvers = require("./resolvers/index")
+const typeDefs = require("./schemas/typdefs");
+const resolvers = require("./resolvers/index");
 connectDb();
 
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  uploads: false,
+});
 
-const  apolloServer  =  new  ApolloServer({  typeDefs,  resolvers, uploads:false });
-const startServer = apolloServer.start()
+const startServer = apolloServer.start();
 
 export default async (req, res) => {
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader(
-        "Access-Control-Allow-Origin",
-        "https://studio.apollographql.com"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Accept, Content-Type, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD"
-    );
-    if (req.method === "OPTIONS") {
-        res.end();
-        return false;
-    }
-
-  const contentType = req.headers['content-type']
-
-
-
-  
-  
-  if (contentType && contentType.startsWith('multipart/form-data')) {
-
-    req.filePayload = await processRequest(req, res)
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://studio.apollographql.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Accept, Content-Type, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD"
+  );
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
   }
 
-  await startServer
+  // Add the `graphqlUploadExpress` middleware
+  await graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 })(req, res, () => {});
 
-  return apolloServer.createHandler({ path: '/api/graphql' })(req, res)
-}
+  const contentType = req.headers["content-type"];
+
+  if (contentType && contentType.startsWith("multipart/form-data")) {
+    req.filePayload = await processRequest(req, res);
+  }
+
+  await startServer;
+
+  return apolloServer.createHandler({ path: "/api/graphql" })(req, res);
+};
 
 export const config = {
-  api: { bodyParser: false }
-}
+  api: { bodyParser: false },
+};
